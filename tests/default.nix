1b2e8aca168198792047f40388311fc4f422074e
Philip Hayes <philiphayes9@gmail.com>
2024-07-10 20:56:04 -0700

    tests: fix linkFarm so we get a separate dir for each item

--- tests/default.nix
+++ tests/default.nix
@@ -3,13 +3,15 @@
   nargoLib,
   pkgs,
 }: let
-  inherit (builtins) catAttrs attrValues;
+  inherit (builtins) attrNames map;
 in rec {
   examples = import ./examples {inherit craneLib nargoLib pkgs;};
 
   # Ensure all examples can run `generateCargoMetadata` successfully.
-  genMetadataAllExamples = pkgs.symlinkJoin {
-    name = "gen-metadata-all";
-    paths = catAttrs "metadata" (attrValues examples);
-  };
+  genMetadataAllExamples = pkgs.linkFarm "gen-metadata-all" (
+    map (name: {
+      name = name;
+      path = examples.${name}.metadata;
+    }) (attrNames examples)
+  );
 }
