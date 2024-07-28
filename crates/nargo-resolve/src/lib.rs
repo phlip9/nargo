f022103a1d7beec57904738742cd85c8be35c3d7
Philip Hayes <philiphayes9@gmail.com>
2024-07-27 18:09:13 -0700

    nargo-resolve: move unit graph input into its own module

--- crates/nargo-resolve/src/lib.rs
+++ crates/nargo-resolve/src/lib.rs
@@ -1,45 +1,6 @@
-// a
+mod unit_graph;
 
-use serde::Deserialize;
-
-#[derive(Deserialize)]
-pub struct UnitGraph<'a> {
-    pub version: u32,
-
-    #[serde(borrow)]
-    pub units: Vec<Unit<'a>>,
-
-    pub roots: Vec<usize>,
-}
-
-#[derive(Deserialize)]
-pub struct Unit<'a> {
-    pub pkg_id: &'a str,
-    pub target: UnitTarget,
-    pub profile: UnitProfile,
-    pub platform: &'a str,
-    pub mode: &'a str,
-    #[serde(borrow)]
-    pub features: Vec<&'a str>,
-    pub dependencies: Vec<UnitDep>,
-}
-
-#[derive(Deserialize)]
-pub struct UnitTarget {
-    // TODO
-}
-
-#[derive(Deserialize)]
-pub struct UnitProfile {
-    // TODO
-}
-
-#[derive(Deserialize)]
-pub struct UnitDep {
-    // TODO
-}
-
-// --- run --- //
+use unit_graph::UnitGraph;
 
 pub fn run(input_bytes: &[u8]) {
     let input: UnitGraph<'_> = serde_json::from_slice(input_bytes)
