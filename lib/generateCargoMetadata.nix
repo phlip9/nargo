599fc82df44d05d27a659ebcf65b5a7527870e9d
Philip Hayes <philiphayes9@gmail.com>
2024-07-27 18:08:31 -0700

    nargo-metadata: remove toml2json. just passthru tomls direct

--- lib/generateCargoMetadata.nix
+++ lib/generateCargoMetadata.nix
@@ -17,48 +17,34 @@
 let
   raw =
     pkgsBuildBuild.runCommandLocal "${name}-cargo-metadata-raw" {
-      depsBuildBuild = [cargo jq toml2json];
-      env.cargoVendorDir = cargoVendorDir;
+      depsBuildBuild = [cargo jq];
+      env = {
+        cargoVendorDir = cargoVendorDir;
+        cargoSrc = "${src}";
+      };
     } ''
       export CARGO_TARGET_DIR="$PWD/target"
       export CARGO_HOME="$PWD/.cargo-home"
-      mkdir -p "$CARGO_HOME"
-      cp "$cargoVendorDir/config.toml" "$CARGO_HOME/config.toml"
+      mkdir "$CARGO_HOME"
+      mkdir "$out"
+      ln -s "$cargoVendorDir/config.toml" "$CARGO_HOME/config.toml"
 
-      mkdir $out
-
-      # phlip9: for now, output these intermediate files as well. makes it easier
+      # phlip9: for now, link these intermediate files as well. makes it easier
       # to debug.
-
-      # Ingest Cargo.lock file
-      local cargoLockJson
-      cargoLockJson="$out/Cargo.lock.json"
-      toml2json --pretty "${src}/Cargo.lock" > $cargoLockJson
-      # cargoLockJson=$(mktemp)
-      # trap "rm $cargoLockJson" 0
-
-      # Ingest vendored registry+gitdep info from 'vendorCargoDeps'
-      local cargoVendorJson
-      cargoVendorJson="$out/Cargo.vendor.json"
-      toml2json --pretty "$cargoVendorDir/config.toml" > $cargoVendorJson
-      # cargoVendorJson=$(mktemp)
-      # trap "rm $cargoVendorJson" 0
-
-      # cp $cargoLockJson $out/Cargo.lock.json
-      # cp $cargoVendorJson $out/Cargo.vendor.json
+      ln -s "$cargoVendorDir/config.toml" "$out/config.toml"
+      ln -s "$cargoSrc/Cargo.lock" "$out/Cargo.lock"
 
       set -x
 
-      printenv cargoVendorDir
-
+      # Generate the `Cargo.metadata.raw.json` file.
       cargo metadata \
-        --manifest-path="${src}/Cargo.toml" \
+        --manifest-path="$cargoSrc/Cargo.toml" \
         --offline \
         --locked \
         --format-version=1 \
         --all-features \
         | jq . \
-        > $out/Cargo.metadata.raw.json
+        > "$out/Cargo.metadata.raw.json"
 
       set +x
     '';
@@ -73,8 +59,8 @@ in
     mkdir $out
 
     # Also include links to these files from raw for easier debugging.
-    ln -s "$raw/Cargo.vendor.json" "$out/Cargo.vendor.json"
-    ln -s "$raw/Cargo.lock.json" "$out/Cargo.lock.json"
+    ln -s "$raw/config.toml" "$out/config.toml"
+    ln -s "$raw/Cargo.lock" "$out/Cargo.lock"
     ln -s "$raw/Cargo.metadata.raw.json" "$out/Cargo.metadata.raw.json"
 
     set -x
