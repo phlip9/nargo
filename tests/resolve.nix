ef4232c1087a5d25612525a9a8f7e3244553f317
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2025-03-10 10:48:08 -0700

    tests: resolve: eval nargo feature resolution with different targets

--- tests/resolve.nix
+++ tests/resolve.nix
@@ -1,10 +1,28 @@
-{nargoLib}: {
-  dumpWorkspace = let
-    metadata = builtins.fromJSON (builtins.readFile ../Cargo.metadata.json);
-    buildTarget = "x86_64-unknown-linux-gnu";
-    hostTarget = "x86_64-unknown-linux-gnu";
-  in
-    nargoLib.resolve.resolveFeatures {
-      inherit metadata buildTarget hostTarget;
-    };
-}
+# Easily debug workspace nargo feature resolution.
+{nargoLib}: let
+  metadata = builtins.fromJSON (builtins.readFile ../Cargo.metadata.json);
+  resolveFeatures = nargoLib.resolve.resolveFeatures;
+  buildTargets = [
+    "x86_64-unknown-linux-gnu"
+    "aarch64-unknown-linux-gnu"
+  ];
+  hostTargets = [
+    "x86_64-unknown-linux-gnu"
+    "aarch64-unknown-linux-gnu"
+    "wasm32-wasi"
+  ];
+in
+  builtins.listToAttrs (builtins.map (buildTarget: {
+      name = buildTarget;
+      value = builtins.listToAttrs (builtins.map (hostTarget: {
+          name = hostTarget;
+          value = resolveFeatures {
+            metadata = metadata;
+            buildTarget = buildTarget;
+            hostTarget = hostTarget;
+            rootPkgIds = ["nargo-metadata"];
+          };
+        })
+        hostTargets);
+    })
+    buildTargets)
